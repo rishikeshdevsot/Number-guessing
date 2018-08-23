40b94e1b0e6dc5fd5127f84a76657b72b32bc03a
rishikeshdevsot <rishikeshdevsot@gmail.com>
2018-08-22 20:13:00 -0400

    Fixed bugs

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ var guesses = document.createElement('p');
 var Result = document.createElement('p');
 
 var attempts = 0;
+var state = 0;
+var restartCreated = false;
 
 guesses.textContent = "Guesses: ";
 document.body.appendChild(guesses);
@@ -16,7 +18,7 @@ var submitButton = document.querySelector("button");
 submitButton.onclick = function() {
     var enteredNumber = Number(document.querySelector("input").value);
     attempts += 1;
-    if(attempts <= 10){
+    if((attempts <= 10)&&(!state)){
         
         if (enteredNumber === randomNum){
             Attempt(1, enteredNumber);
@@ -24,6 +26,9 @@ submitButton.onclick = function() {
         else{
             Attempt(0, enteredNumber);
         }
+    }
+    else if(state){
+        gameOver(state);
     }
     else{
         gameOver(state=0);
@@ -45,27 +50,37 @@ function lowOrHigh(entry){
 
 function gameOver(state){
    if(!state){
-        Result.textContent = "GameOver :(";
-        var restart = document.createElement("button");
-        restart.textContent = "Restart";
-        document.body.appendChild(restart);
-        restart.onclick = function(){
-            attempts = 0;
-            document.body.removeChild(restart);
-            guesses.textContent = "Guesses: "
-            Result.textContent = "";
+       if(!restartCreated){
+            Result.textContent = "GameOver :(";
+            var restart = document.createElement("button");
+            restart.textContent = "Restart";
+            document.body.appendChild(restart);
+            restartCreated = true;
+            restart.onclick = function(){
+                attempts = 0;
+                document.body.removeChild(restart);
+                guesses.textContent = "Guesses: "
+                Result.textContent = "";
+                var randomNum = Math.floor((Math.random()*1000) + 1);
+                restartCreated = false;
+            }
         }
     }
     else{
-        var restart = document.createElement("button");
-        restart.textContent = "Restart";
-        document.body.appendChild(restart);
-        restart.onclick = function(){
-            attempts = 0;
-            document.body.removeChild(restart);
-            guesses.textContent = "Guesses: "
-            Result.textContent = "";
+        if(!restartCreated){
+            var restart = document.createElement("button");
+            restart.textContent = "Restart";
+            document.body.appendChild(restart);
+            restartCreated = true;
+            restart.onclick = function(){
+                attempts = 0;
+                document.body.removeChild(restart);
+                guesses.textContent = "Guesses: "
+                Result.textContent = "";
+                var randomNum = Math.floor((Math.random()*1000) + 1);
+                restartCreated = false;
 
+            }
         }
     }
 
